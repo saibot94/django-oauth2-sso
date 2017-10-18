af5822db88e20e1bf8bf2bb24f6c6d0a631460e3
Cristian Schuszter <cristian.schuszter@cern.ch>
2017-10-18 15:40:29 +0200

    Change setup a bit

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
 
         'License :: OSI Approved :: MIT License',
 
-        'Framework :: Django :: 1.5',
         'Framework :: Django :: 1.6',
         'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
@@ -46,10 +45,8 @@ setup(
         'Framework :: Django :: 1.10',
 
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
